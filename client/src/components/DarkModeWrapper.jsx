c0709c1eca2f4e4235f4734bb553daa2b1887131
Manojkumar-05 <manojkumar.cb22@bitsahty.ac.in>
2024-04-30 20:09:46 +0530

    2024-04-30T20:09

--- client/src/components/DarkModeWrapper.jsx
+++ client/src/components/DarkModeWrapper.jsx
@@ -2,7 +2,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import { useDark } from "./store/darkStore";
 
 const DarkModeWrapper = ({ children }) => {
-  const { dark, toggleDark } = useDark();
+  const { dark } = useDark();
 
   return <NextUIProvider class={dark && "dark"}>{children}</NextUIProvider>;
 };
